c881513282244d59a958b1d38b3b8cef81a13967
andytorrestb <andytorrestb@gmail.com>
2020-09-15 23:22:37 -0400

    add mesh for vonKarman nose cone

---
+++ 2020/fall/week-04/bullet/dynamicCode/_3d238ef788875adeb68122ea126663dc1c11b2d3/codeStreamTemplate.C
@@ -0,0 +1,97 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) YEAR AUTHOR, AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+#include "pointField.H"
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C" void codeStream_3d238ef788875adeb68122ea126663dc1c11b2d3(Ostream& os, const dictionary& dict)
+{
+//{{{ begin code
+    pointField points(19);
+        points[0]  = point(25, 0, -0.5);
+        points[1] = point(26, 0, -0.5);
+        points[2] = point(26, 3.74, -0.5);
+        points[3] = point(25, 3.74, -0.5);
+        points[4] = point(25, 4.74, -0.5);
+        points[5] = point(26, 4.74, -0.5);
+        points[6] = point(26, 5.74, -0.5);
+        points[7] = point(25, 5.74, -0.5);
+        points[8] = point(24, 5.74, -0.5);
+        points[9] = point(24, 4.74, -0.5);
+        points[10] = point(24, 3.74, -0.5); // end of nozzle
+        points[11] = point(12.8, 2.58, -0.5);
+        points[12] = point(11.72, 3.66, -0.5); // midpoint
+        points[13] = point(11.72, 5.74, -0.5);
+        points[14] = point(0, 5.74, -0.5);
+        points[15] = point(0, 3.66, -0.5);
+        points[16] = point(0, 0, -0.5);
+        points[17] = point(1, 0, -0.5);
+        points[18] = point(2, 0, -0.5); // end of nozzle
+
+        // Duplicate z points
+        label sz = points.size();
+        points.setSize(2*sz);
+        for (label i = 0; i < sz; i++)
+        {
+            const point& pt = points[i];
+            points[i+sz] = point(pt.x(), pt.y(), -pt.z());
+        }
+
+        os  << points;
+//}}} end code
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
