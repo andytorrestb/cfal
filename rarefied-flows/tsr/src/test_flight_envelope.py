c9393eabd18088c46fabb2673d73dadfbc5ff8f8
andytorrestb <andytorrestb@gmail.com>
2021-04-21 00:47:43 -0400

    add foundation for unit tests

---
+++ rarefied-flows/tsr/src/test_flight_envelope.py
@@ -0,0 +1,12 @@
+import unittest
+import flight_envelope
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        result  = flight_envelope.add(10, 5)
+        self.assertEqual(result, 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
