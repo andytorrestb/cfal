8bfdca6f24f2ffee7ed068c581739c61623215c8
Andy Torres <andy9371k@knights.ucf.edu>
2021-04-21 06:12:53 +0000

    add setup envelope

--- rarefied-flows/tsr/src/flight_envelope.py
+++ rarefied-flows/tsr/src/flight_envelope.py
@@ -1,24 +1,41 @@
 import numpy as np
+import pandas as pd
 
 class FlightEnvelope:
     """Holds data structures and methods for producing simulation data for a given flight envelope"""
 
+
     def __init__(self):
 
+        # Set up envelope parameters. 
         print('Set Altitude Values')
         h_lower = int(input('Lower Bound '))
         h_upper = int(input('Upper Bound '))
         h_values = int(input('Number of Values '))
         self.h = list(np.linspace(h_lower, h_upper, h_values))
 
-        
         print('Set Mach Numbers Values')
         Ma_lower = float(input('Lower Bound '))
         Ma_upper = float(input('Upper Bound '))
         Ma_values = int(input('Number of Values '))
-        self.Ma = list(np.arange(Ma_lower, Ma_upper, Ma_values))
+        self.Ma = list(np.linspace(Ma_lower, Ma_upper, Ma_values))
 
-        
         self.f = int(input('Enter level of mesh fidelity '))
         self.t = float(input('Enter simulation duration '))
+
+    # Set up dataframe that holds the unique cases. 
+    def setup_envelope(self):
+
+      labels = pd.Index(['Ma', 'h', 'T', 'P', 'rho', 'u', 'Re', 'Kn', 'data'])
+      U = len(self.Ma) * len(self.h)
+
+      print(len(self.Ma), len(self.h))
+      print(U)
+      self.df = pd.DataFrame(np.zeros(shape = (len(fe.Ma) * len(fe.h), 9)))
+      self.df.columns = labels
+
+
+fe = FlightEnvelope()
+fe.setup_envelope()
+print(fe.df)
 
--- rarefied-flows/tsr/src/test_flight_envelope.py
+++ rarefied-flows/tsr/src/test_flight_envelope.py
@@ -1,4 +1,5 @@
 import unittest
+import pandas as pdpip
 from flight_envelope import FlightEnvelope
 
 class TestFlightEnvelope(unittest.TestCase):
@@ -20,6 +21,19 @@ class TestFlightEnvelope(unittest.TestCase):
         self.assertIsInstance(self.fe_1.Ma, list, '')
         self.assertIsInstance(self.fe_1.f, int, '')
         self.assertIsInstance(self.fe_1.t, float, '')
+
+    def test_setup_envelope(self):
+      
+      fe = self.fe_1
+      fe.setup_envelope()
+
+      labels = pd.Index(['Ma', 'h', 'T', 'P', 'rho', 'u', 'Re', 'Kn', 'data'])
+      U = (len(fe.Ma * fe.h), len(labels))
+
+      self.assertIsInstance(fe.df, pd.DataFrame, '')
+      self.assertEqual(U, fe.df.columns)
+      self.assertEqual(labels, fe.df.shape)
+
 
 
 if __name__ == '__main__':
