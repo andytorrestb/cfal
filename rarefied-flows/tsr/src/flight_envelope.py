ca96a64995040ec254c2c89b83d61bf3d3ee4fe1
Andy Torres <andy9371k@knights.ucf.edu>
2021-04-21 05:10:14 +0000

    Debug init function to pass initial test case

--- rarefied-flows/tsr/src/flight_envelope.py
+++ rarefied-flows/tsr/src/flight_envelope.py
@@ -1,2 +1,24 @@
-def add(x, y):
-    return x + y
+import numpy as np
+
+class FlightEnvelope:
+    """Holds data structures and methods for producing simulation data for a given flight envelope"""
+
+    def __init__(self):
+
+        print('Set Altitude Values')
+        h_lower = int(input('Lower Bound '))
+        h_upper = int(input('Upper Bound '))
+        h_values = int(input('Number of Values '))
+        self.h = list(np.linspace(h_lower, h_upper, h_values))
+
+        
+        print('Set Mach Numbers Values')
+        Ma_lower = float(input('Lower Bound '))
+        Ma_upper = float(input('Upper Bound '))
+        Ma_values = int(input('Number of Values '))
+        self.Ma = list(np.arange(Ma_lower, Ma_upper, Ma_values))
+
+        
+        self.f = int(input('Enter level of mesh fidelity '))
+        self.t = float(input('Enter simulation duration '))
+
