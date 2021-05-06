f0e3aee4a925458a527ed7253fb16af4f1a8c65b
andytorrestb <andytorrestb@gmail.com>
2021-05-06 19:47:57 -0400

    update test_meta_data to check for a range of dataframe sizes

--- rarefied-flows/src/test_meta_data_generator.py
+++ rarefied-flows/src/test_meta_data_generator.py
@@ -4,17 +4,23 @@ from meta_data_generator import MetaDataGenerator
 class TestMetaDataGenerator(unittest.TestCase):
 
     def test_innit(self):
-        Ma_params = { 'lower' : 1, 'upper' : 5, 'vals' : 10 }
-        h_params = { 'lower' : 1000, 'upper' : 100000, 'vals' : 10 }
-        L_params = { 'lower' : 1, 'upper' : 3, 'vals' : 3 - 1 + 1 }
-        H_params = { 'lower' : 1, 'upper' : 1, 'vals' : 3 - 1 + 1 }
-        params = { 'Ma' : Ma_params, 'h' : h_params, 'L' : L_params, 'H' : H_params }
 
-        data = MetaDataGenerator(params)
+        for n_vals in range(9):
+            Ma_params = { 'lower' : 1, 'upper' : 5, 'vals' : n_vals }
+            h_params = { 'lower' : 1000, 'upper' : 100000, 'vals' : n_vals }
+            L_params = { 'lower' : 1, 'upper' : 3, 'vals' :  n_vals }
+            H_params = { 'lower' : 1, 'upper' : 1, 'vals' : n_vals }
+            params = { 'Ma' : Ma_params, 'h' : h_params,'L' : L_params, 'H' : H_params }
 
-        self.assertEqual(data.df.shape, (1500, 9))
+            data = MetaDataGenerator(params)
 
+            self.assertEqual(len(data.Ma_range), Ma_params['vals'])   
+            self.assertEqual(len(data.h_range), h_params['vals'])
+            self.assertEqual(len(data.L_range), L_params['vals'])     
+            self.assertEqual(len(data.H_range), H_params['vals'])
+            self.assertEqual(data.df.shape, (5 * n_vals ** 3, 9))
 
+            
 
 if __name__ == '__main__':
     unittest.main()
